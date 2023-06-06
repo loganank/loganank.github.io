499975150cf97189d532979ba05d6c16627d50e3
loganank <ldankarberg@gmail.com>
2023-06-06 12:16:15 -0600

    after following tutorial and making some changes

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+const Hero = ({image_path}) => (
+    <section class="hero" id="about">
+        <img
+            src={image_path}
+            alt="hero-img"
+            loading="lazy"
+            class="hero-img"
+        />
+        <div class="bio animate__animated animate__shakeX">
+            <h2 class="bio-title">About Me</h2>
+            <p class="bio-text">
+            This is the example text about Logan Ankarberg. He is a nerdy man
+            who eats chips and plays valorant. When not doing that, he is 
+            tinkering with machine learning models and making new things.
+            </p>
+        </div>
+    </section>
+)
+
+export {Hero}
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+
+const Nav = ({title}) => (
+    <nav>
+        <h1>{title}</h1>
+        <ul class="navigation">
+        <li><a href="#about" class="nav-link">About</a></li>
+        <li><a href="#skills" class="nav-link">Skills</a></li>
+        <li><a href="#projects" class="nav-link">Projects</a></li>
+        <li><a href="#contact" class="nav-link">Contact</a></li>
+        </ul>
+        <button class="burger-menu" id="burger-menu">
+        <ion-icon class="bars" name="menu-outline"></ion-icon>
+        </button>
+    </nav>
+);
+
+export {Nav};
